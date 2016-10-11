d98a90af58648d7c3f49e5d0ef9fd31fc8414807
Thelma Boamah <thelma.a.boamah@gmail.com>
2016-10-10 21:19:31 -0400

    algorithms

---
+++ algorithms.js
@@ -0,0 +1,59 @@
+//Reverse the provided string.
+
+function reverseString(str) {  
+  return str.split("").reverse().join("");
+}
+
+
+
+
+
+
+
+/* Return the factorial of the provided integer.
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+Factorials are often represented with the shorthand notation n!
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+*/
+function factorialize(num) {
+  var total = 1; 
+  for (var i = num; i>= 1; i--) {
+    total*=i;
+  }
+  return total;
+  
+}
+
+
+
+//Return true if the given string is a palindrome. Otherwise, return false.
+function palindrome(str) {
+  // Good luck!
+  str = str.replace(/[\s"'.,-\/#!$%\^&*;:{}=\-_`~()\\\[\]@+|?><]/g,"").toLowerCase();
+  var reverseStr = str.split("").reverse().join("");
+  
+  if (str === reverseStr) {
+  	return true;
+  } else {
+  	return false;
+  }
+  
+}
+
+
+
+//Return the length of the longest word in the provided sentence.
+function findLongestWord(str) {
+  var strArray = str.split(" "); 
+  var strLengthArray = [];
+  
+  for( var i = 0; i < strArray.length; i++) {
+  	strLengthArray.push(strArray[i].length);
+  }
+  
+  strLengthArray.sort(function(a,b) {
+  	return b - a;
+  });
+  
+  return strLengthArray[0]; 
+}
