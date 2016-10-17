5ef7918b4a48c6d51eed562b103828bad3fcbe89
Thelma Boamah <thelma.a.boamah@gmail.com>
2016-10-17 08:55:08 -0400

    slasher algo

--- algorithms.js
+++ algorithms.js
@@ -141,3 +141,12 @@ function chunkArrayInGroups(arr, size) {
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+//Return the remaining elements of an array after chopping off n elements from the head.
+function slasher(arr, howMany) {
+  // it doesn't always pay to be first
+    arr = arr.slice(howMany);
+  return arr;
+}
+
+slasher([1, 2, 3], 2);
